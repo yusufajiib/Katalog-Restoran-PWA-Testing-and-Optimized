9301ca0b6beb4f9b4e1d3156ef8b961494c0350f
yusufajiib <51849143+yusufajiib@users.noreply.github.com>
2023-04-13 18:33:04 +0700

    Add files via upload

---
+++ src/scripts/views/app.js
@@ -0,0 +1,40 @@
+import DrawerInitiator from '../utils/drawer-initiator';
+import UrlParser from '../routes/url-parser';
+import routes from '../routes/routes';
+
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+
+    this._initialAppShell();
+  }
+
+  _initialAppShell() {
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content,
+    });
+
+    // kita bisa menginisiasikan komponen lain bila ada
+  }
+
+  async renderPage() {
+    const url = UrlParser.parseActiveUrlWithCombiner();
+    const page = routes[url];
+    this._content.innerHTML = await page.render();
+    await page.afterRender();
+    // Agar fungsi skip-to-content bisa berjalan dengan baik
+    const mainContent = document.querySelector('#mainContent');
+    const skipLink = document.querySelector('.skip-link');
+    skipLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      mainContent.scrollIntoView({ behavior: 'smooth' });
+      skipLink.blur();
+    });
+  }
+}
+
+export default App;
